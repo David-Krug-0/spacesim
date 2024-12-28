4f0ad033db97248aac99a61c89e209b2bad0878b
David-Krug-0 <magicalwario@gmail.com>
2024-12-28 13:55:22 -0700

    Revert "Randomize seeds"
    This reverts commit 0fcb5523e3d878cff81b579378fab04be6f2d132.

--- Space/src/com/space/sim/universe/_2d/Universe2D.java
+++ Space/src/com/space/sim/universe/_2d/Universe2D.java
@@ -122,12 +122,14 @@ public class Universe2D extends Universe<Vector2> {
             case 7:
                 // Dust
             	Random rand = new Random();
-            	long seed = System.nanoTime();
+            	long seed = //System.nanoTime();
+            			98425323328200l;
             	System.out.println("Start Seed: " + seed);
             	rand.setSeed(seed);
             	
             	Random colRand = new Random();
-            	long colSeed = System.nanoTime();
+            	long colSeed = //System.nanoTime();
+            			98425323550500l;
             	System.out.println("Color Seed: " + colSeed);
             	colRand.setSeed(colSeed);
             	
